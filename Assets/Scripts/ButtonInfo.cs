a22200508295eb09bea77431f4ae527a81608154
Kobruske <kobruske@gmail.com>
2024-03-27 13:21:40 +0200

    Updated shop and added a money clicker

---
+++ Assets/Scripts/ButtonInfo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInfo : MonoBehaviour
+{
+    public int ItemID;
+    public TMP_Text PriceTXT;
+    public TMP_Text QuantityTXT;
+    public GameObject ShopManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PriceTXT.text = "Price: $" + ShopManager.GetComponent<ShopManagerScript>().shopItems[2, ItemID].ToString();
+        QuantityTXT.text = "Quantity: " + ShopManager.GetComponent<ShopManagerScript>().shopItems[3, ItemID].ToString();
+    }
+}
---
+++ Assets/Scripts/ShopManagerScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopManagerScript : MonoBehaviour
+{
+    public int[,] shopItems = new int[5,5];
+    public TextMeshProUGUI MoneyTXTnumber;
+    public CoinClicker MoneyClicker;
+    void Start()
+    {
+        //MoneyTXTnumber.text = MoneyClicker.money.ToString();
+        // ID
+        shopItems[1, 1] = 1;
+        shopItems[1, 2] = 2;
+        shopItems[1, 3] = 3;
+        shopItems[1, 4] = 4;
+        // Price
+        shopItems[2, 1] = 10;
+        shopItems[2, 2] = 20;
+        shopItems[2, 3] = 30;
+        shopItems[2, 4] = 40;
+        //Quantity
+        shopItems[3, 1] = 5;
+        shopItems[3, 2] = 5;
+        shopItems[3, 3] = 5;
+        shopItems[3, 4] = 5;
+    }
+    public void Buy()
+    {
+        GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
+        
+        if (MoneyClicker.money >= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().ItemID] && shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID] > 0)
+        {
+            MoneyClicker.money -= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().ItemID];
+            shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID]--;
+            MoneyTXTnumber.text = MoneyClicker.money.ToString();
+            ButtonRef.GetComponent<ButtonInfo>().QuantityTXT.text = shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID].ToString();
+        }
+    }
+}
